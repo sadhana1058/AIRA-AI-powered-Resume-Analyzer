f7945e772576fac1baecd8eab7b746d1ec05b740
sadhana1058 <sadhana1058@gmail.com>
2025-10-05 18:33:39 -0400

    added drag and drop

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import {useDropzone} from 'react-dropzone'
 
 const FileUploader = () => {
+    const onDrop = useCallback(acceptedFiles => {
+        // Do something with the files
+      }, [])
+      const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
   return (
     <div className="w-full gradient-border">
-      
+      <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop your resume here, or click to select files</p>
+      }
+    </div>
     </div>
   )
 }
