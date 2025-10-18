e2f1a9e926c1396b1b149f8b12fd4c8b70396339
sadhana1058 <sadhana1058@gmail.com>
2025-10-18 16:58:07 -0400

    file

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -2,9 +2,7 @@ import React, { useCallback } from 'react'
 import {useDropzone} from 'react-dropzone'
 
 const FileUploader = () => {
-    const onDrop = useCallback(acceptedFiles => {
-        // Do something with the files
-      }, [])
+
       const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
   return (
     <div className="w-full gradient-border">
