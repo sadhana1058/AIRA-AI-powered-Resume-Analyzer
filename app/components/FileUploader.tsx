730cc0f50d6a69eedd97ffcf01fdf9dc644ab130
sadhana1058 <sadhana1058@gmail.com>
2025-10-18 17:44:34 -0400

    upload docs or pdf

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -1,9 +1,30 @@
 import React, { useCallback } from 'react'
 import {useDropzone} from 'react-dropzone'
+interface FileUploaderProps {
+  onFileSelect?: (file: File | null) => void;
+}
 
-const FileUploader = () => {
+const FileUploader =  ({ onFileSelect }: FileUploaderProps) => {
 
-      const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+      
+      const onDrop = useCallback((acceptedFiles: File[]) => {
+        const file = acceptedFiles[0] || null;
+
+        onFileSelect?.(file);
+    }, [onFileSelect]);
+
+    const maxFileSize = 20 * 1024 * 1024; // 20MB in bytes
+
+    const {getRootProps, getInputProps, isDragActive, acceptedFiles} = useDropzone({
+        onDrop,
+        multiple: false,
+        accept: { 'application/pdf': ['.pdf'],  'application/msword': ['.doc'],
+          'application/vnd.openxmlformats-officedocument.wordprocessingml.document': ['.docx']
+        },
+        maxSize: maxFileSize,
+    })
+
+    const file = acceptedFiles[0] || null;
   return (
     <div className="w-full gradient-border">
       <div {...getRootProps()}>
